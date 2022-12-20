e0f9bbde0261a83b6652c2be326e2086890de541
yheechan <heechy06@naver.com>
2022-12-20 22:59:18 +0900

    Initialized metainfo on data of version3

--- src/initData.py
+++ src/initData.py
@@ -1,5 +1,6 @@
 import recorder
 
+version = 'version1'
 proj_list =[
     'boringssl', 'c-ares',
     'freetype2', 'guetzli',
@@ -11,4 +12,15 @@ proj_list =[
     'wpantund'
 ]
 
-recorder.recordData(proj_list)
+version = 'version3'
+proj_list = [
+    'total_aspell', 'total_boringssl', 'total_c-ares', 'total_exiv2',
+    'total_freetype2', 'total_grok', 'total_guetzli', 'total_harfbuzz',
+    'total_lcms', 'total_libarchive', 'total_libexif', 'total_libhtp',
+    'total_libpng', 'total_libsndfile', 'total_libssh', 'total_libxml2',
+    'total_ndpi', 'total_openthread', 'total_pcre2', 'total_proj4',
+    'total_re2', 'total_sqlite3', 'total_usrsctp', 'total_vorbis',
+    'total_woff2', 'total_wpantund', 'total_yara', 'total_zstd'
+]
+
+recorder.recordData(proj_list, version)
--- src/recorder.py
+++ src/recorder.py
@@ -1,8 +1,6 @@
 import json
 
-
-
-def recordData(proj_list):
+def recordData(proj_list, version):
 
     token_choices = []
     max_len = -1
@@ -21,26 +19,26 @@ def recordData(proj_list):
     for fn in proj_list:
         print('checking project \"' + fn + '\"')
 
-        with open('../data/'+fn, 'r') as f:
+        with open('../data/'+version+'/'+fn, 'r') as f:
             lines = f.readlines()
         
-
         for line in lines:
-            json_data = json.loads(line.rstrip().replace("\'", "\""))
 
-            if str(json_data['label-type'][0]) not in token_choices:
+            json_data = json.loads(line)
+
+            if int(json_data['label-type'][0]) not in token_choices:
                 print('added choice \"' + str(json_data['label-type'][0]) + '\"')
-                token_choices.append(str(json_data['label-type'][0]))
+                token_choices.append(int(json_data['label-type'][0]))
 
             for tok in json_data['prefix']:
-                if str(tok) not in token_choices:
+                if int(tok) not in token_choices:
                     print('added choice \"' + str(tok) + '\"')
-                    token_choices.append(str(tok))
+                    token_choices.append(int(tok))
             
             for tok in json_data['postfix']:
-                if str(tok) not in token_choices:
+                if int(tok) not in token_choices:
                     print('added choice \"' + str(tok) + '\"')
-                    token_choices.append(str(tok))
+                    token_choices.append(int(tok))
             
             if len(json_data['prefix']) > max_len: max_len = len(json_data['prefix'])
             if len(json_data['postfix']) > max_len: max_len = len(json_data['postfix'])
@@ -61,10 +59,12 @@ def recordData(proj_list):
                 else:
                     total_ones_postfix[cnt] += 1
 
-        
         project_data.append(fn + ': \t\t' + str(len(lines)))
 
-    token_choices.append('213')
+    # token_choices.append('213')
+
+    token_choices.sort()
+    token_choices = list(map(str, token_choices))
     
     record_list('../record/project_data', project_data)
     record_list('../record/token_choices', token_choices)
